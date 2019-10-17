8f51508f81a2f88836112f00efc545c330586aef
Falconwatch <garrikwower@gmail.com>
2019-10-17 20:33:43 +0300

    stat w1, algo w1 t2

--- Algo/w1/Task1/Task2_2/Source.cpp
+++ Algo/w1/Task1/Task2_2/Source.cpp
@@ -1,8 +1,50 @@
 #include <iostream>
 
+class Heap {
+	public:
+		Heap();
+		~Heap();
+
+		void make_heap();
+
+		void push_heap() {
+
+		}
+
+		int pop_heap();
+
+		void sort_heap();
+
+		void reverse();
+
+		int getParrentIdx(int childIdx)
+		{
+			return int((childIdx - 1) / 2);
+		}
+		
+		int getLeftChildIdx(int parrentIdx) {
+			return 2 * parrentIdx + 1;
+		}
+
+		int getRightChildIdx(int parrentIdx) {
+			return 2 * parrentIdx + 2;
+		}
+
+private:
+	int* data;
+
+};
+
+
+
 int main() {
 	int n;
 	std::cin >> n;
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++) {
+		std::cin >> arr[i];
+	}
 
+	delete[] arr;
 	return 0;
 }

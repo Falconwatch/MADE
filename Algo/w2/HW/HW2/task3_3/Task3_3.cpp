5fc1df943f583623dc7f1a3df1f736145dbe2020
Falconwatch <garrikwower@gmail.com>
2019-10-26 23:34:52 +0300

    Working

--- Algo/w2/HW/HW2/task3_3/Task3_3.cpp
+++ Algo/w2/HW/HW2/task3_3/Task3_3.cpp
@@ -1,85 +1,87 @@
-/*
-Автор: Щербаков Игорь MADE-11
-3_3. Количество инверсий.
-Дана последовательность целых чисел из диапазона (-10^9 .. 10^9). Длина последовательности не больше 10^6. Числа записаны по одному в строке. Количество чисел не указано.  
-Пусть количество элементов n, и числа записаны в массиве a = a[i]: i из [0..n-1].
-Требуется напечатать количество таких пар индексов (i,j) из [0..n-1], что (i < j и a[i] > a[j]).
-Указание: количество инверсий может быть больше 4*10^9 - используйте int64_t.
+п»ї/*
+РђРІС‚РѕСЂ: Р©РµСЂР±Р°РєРѕРІ РРіРѕСЂСЊ MADE-11
+3_3. РљРѕР»РёС‡РµСЃС‚РІРѕ РёРЅРІРµСЂСЃРёР№.
+Р”Р°РЅР° РїРѕСЃР»РµРґРѕРІР°С‚РµР»СЊРЅРѕСЃС‚СЊ С†РµР»С‹С… С‡РёСЃРµР» РёР· РґРёР°РїР°Р·РѕРЅР° (-10^9 .. 10^9). Р”Р»РёРЅР° РїРѕСЃР»РµРґРѕРІР°С‚РµР»СЊРЅРѕСЃС‚Рё РЅРµ Р±РѕР»СЊС€Рµ 10^6. Р§РёСЃР»Р° Р·Р°РїРёСЃР°РЅС‹ РїРѕ РѕРґРЅРѕРјСѓ РІ СЃС‚СЂРѕРєРµ. РљРѕР»РёС‡РµСЃС‚РІРѕ С‡РёСЃРµР» РЅРµ СѓРєР°Р·Р°РЅРѕ.  
+РџСѓСЃС‚СЊ РєРѕР»РёС‡РµСЃС‚РІРѕ СЌР»РµРјРµРЅС‚РѕРІ n, Рё С‡РёСЃР»Р° Р·Р°РїРёСЃР°РЅС‹ РІ РјР°СЃСЃРёРІРµ a = a[i]: i РёР· [0..n-1].
+РўСЂРµР±СѓРµС‚СЃСЏ РЅР°РїРµС‡Р°С‚Р°С‚СЊ РєРѕР»РёС‡РµСЃС‚РІРѕ С‚Р°РєРёС… РїР°СЂ РёРЅРґРµРєСЃРѕРІ (i,j) РёР· [0..n-1], С‡С‚Рѕ (i < j Рё a[i] > a[j]).
+РЈРєР°Р·Р°РЅРёРµ: РєРѕР»РёС‡РµСЃС‚РІРѕ РёРЅРІРµСЂСЃРёР№ РјРѕР¶РµС‚ Р±С‹С‚СЊ Р±РѕР»СЊС€Рµ 4*10^9 - РёСЃРїРѕР»СЊР·СѓР№С‚Рµ int64_t.
 */
-
 #include <iostream>
 #include <stdint.h>
 #include <sstream>
 
 using namespace std;
 
-class Sequence {
-public:
-	Sequence();
-	~Sequence();
+#pragma region Р”РёРЅР°РјРёС‡РµСЃРєРёР№_РјР°СЃСЃРёРІ
 
-	//добавить элемент в последовательность
-	void AddElement(int element);
-	//Подсчитывает количество инверсий
-	int64_t CountInversions();
-	//вывести данные из буфера
-	void Show();
-
-private:
-	int* data_;
-	int tail_;
-	int size_;
-	//Увеличть буфер
-	void Enlarge();
-	//Слияние с подсчётом инверсий
-	int64_t ModifiedMerge(int arr[], int l, int m, int r);
-	//Сортировка слиянием с подсчётом инверсий
-	int64_t ModifiedMergeSort(int arr[], int l, int r);
+/*РЎРІРѕР№ РґРёРЅР°РјРёС‡РµСЃРєРёР№ РјР°СЃСЃРёРІ*/
+class DynamicArr {
+	public:
+		DynamicArr();
+		~DynamicArr();
+		void Append(int value);
+		int* GetArray();
+		int GetSize();
+	private:
+		int* data_;
+		int capacity_;
+		int tail_;
+		void Enlarge();
 };
 
-#pragma region SequenceImplementation
-Sequence::Sequence() : tail_(0), size_(0), data_(nullptr) {
-	Enlarge();
+DynamicArr::DynamicArr(): capacity_(8), tail_(0){
+	data_ = new int[capacity_];
 }
 
-Sequence::~Sequence() {
-	if (data_!=nullptr) delete[] data_;
-}
-
-void Sequence::Enlarge() {
-	if (size_ > 0) {
-		int newSize = size_ * 2;
-		int* newData = new int[newSize];
-		for (int i = 0; i < tail_; i++) {
-			newData[i] = data_[i];
-		}
-		if (data_ != nullptr) delete[] data_;
-		data_ = newData;
-		size_ = newSize;
-	}
-	else {
-		size_ = 8;
-		data_ = new int[size_];
-		tail_ = 0;
+DynamicArr::~DynamicArr() {
+	if (data_ != nullptr) {
+		delete[] data_;
 	}
 }
 
-int64_t Sequence::ModifiedMerge(int arr[], int l, int m, int r)
+void DynamicArr::Append(int value) {
+	if (tail_ == capacity_) {
+		Enlarge();
+	}
+	data_[tail_] = value;
+	tail_++;
+}
+
+void DynamicArr::Enlarge() {
+	int new_capacity = capacity_ * 2;
+	int* new_data = new int[new_capacity];
+	for (int i = 0; i < tail_; i++) {
+		new_data[i] = data_[i];
+	}
+	delete data_;
+	data_ = new_data;
+}
+
+int* DynamicArr::GetArray() {
+	return data_;
+}
+
+int DynamicArr::GetSize() {
+	return tail_-1;
+}
+#pragma endregion
+
+int64_t ModifiedMerge(int* arr, int l, int m, int r)
 {
 	int64_t inverses = 0;
 	int i, j, k;
 	int left_part_size = m - l + 1;
 	int right_part_size = r - m;
-	//создааём два подмассива
+	//СЃРѕР·РґР°Р°С‘Рј РґРІР° РїРѕРґРјР°СЃСЃРёРІР°
 	int* L = new int[left_part_size];
 	int* R = new int[right_part_size];
-	//заполняем два подмассива
+	//Р·Р°РїРѕР»РЅСЏРµРј РґРІР° РїРѕРґРјР°СЃСЃРёРІР°
 	for (i = 0; i < left_part_size; i++)
 		L[i] = arr[l + i];
 	for (j = 0; j < right_part_size; j++)
 		R[j] = arr[m + 1 + j];
 
-	//сливаем два подмассива
+	//СЃР»РёРІР°РµРј РґРІР° РїРѕРґРјР°СЃСЃРёРІР°
 	i = 0;
 	j = 0;
 	k = l;
@@ -90,7 +92,7 @@ int64_t Sequence::ModifiedMerge(int arr[], int l, int m, int r)
 			i++;
 		}
 		else {
-			//Считаем скоько чисел из левого массива прошли, сдвигая элемент из правого массива в начало
+			//РЎС‡РёС‚Р°РµРј СЃРєРѕСЊРєРѕ С‡РёСЃРµР» РёР· Р»РµРІРѕРіРѕ РјР°СЃСЃРёРІР° РїСЂРѕС€Р»Рё, СЃРґРІРёРіР°СЏ СЌР»РµРјРµРЅС‚ РёР· РїСЂР°РІРѕРіРѕ РјР°СЃСЃРёРІР° РІ РЅР°С‡Р°Р»Рѕ
 			inverses += left_part_size - i;
 			arr[k] = R[j];
 			j++;
@@ -98,7 +100,7 @@ int64_t Sequence::ModifiedMerge(int arr[], int l, int m, int r)
 		k++;
 	}
 
-	//сливаем  части в один массив
+	//СЃР»РёРІР°РµРј  С‡Р°СЃС‚Рё РІ РѕРґРёРЅ РјР°СЃСЃРёРІ
 	while (i < left_part_size) {
 		arr[k] = L[i];
 		i++;
@@ -109,61 +111,47 @@ int64_t Sequence::ModifiedMerge(int arr[], int l, int m, int r)
 		j++;
 		k++;
 	}
-	//чистим за собой
+	//С‡РёСЃС‚РёРј Р·Р° СЃРѕР±РѕР№
 	delete[] L;
 	delete[] R;
 	return inverses;
 }
 
-int64_t Sequence::ModifiedMergeSort(int arr[], int l, int r)
+int64_t ModifiedMergeSort(int arr[], int l, int r)
 {
 	int64_t leftInverses = 0, rightInverses = 0, currentInverses = 0;
 	if (l < r) {
 		int m = l + (r - l) / 2;
-		// Сортируем две части
+		// РЎРѕСЂС‚РёСЂСѓРµРј РґРІРµ С‡Р°СЃС‚Рё
 		leftInverses = ModifiedMergeSort(arr, l, m);
 		rightInverses = ModifiedMergeSort(arr, m + 1, r);
-		//считаем количество инверсий в текущем мердже
+		//СЃС‡РёС‚Р°РµРј РєРѕР»РёС‡РµСЃС‚РІРѕ РёРЅРІРµСЂСЃРёР№ РІ С‚РµРєСѓС‰РµРј РјРµСЂРґР¶Рµ
 		currentInverses = ModifiedMerge(arr, l, m, r);
 	}
-	//результат = инверсии в первой половине + инверсии во второй половине + инверсии при их слиянии
+	//СЂРµР·СѓР»СЊС‚Р°С‚ = РёРЅРІРµСЂСЃРёРё РІ РїРµСЂРІРѕР№ РїРѕР»РѕРІРёРЅРµ + РёРЅРІРµСЂСЃРёРё РІРѕ РІС‚РѕСЂРѕР№ РїРѕР»РѕРІРёРЅРµ + РёРЅРІРµСЂСЃРёРё РїСЂРё РёС… СЃР»РёСЏРЅРёРё
 	return leftInverses + rightInverses + currentInverses;
 }
 
-void Sequence::Show() {
-	for (int i = 0; i < tail_; i++) 
-		cout << data_[i];
-}
-
-int64_t Sequence::CountInversions() {
-	return ModifiedMergeSort(data_, 0, tail_ - 1);
-}
-
-void Sequence::AddElement(int element) {
-	if (tail_ == size_)
-		Enlarge();
-	data_[tail_] = element;
-	tail_++;
-}
-#pragma endregion
-
 int main() {
 	int64_t cnt = 0;
-	Sequence* mySeq = new Sequence();
-	//Читаем ввод
+	//Р§РёС‚Р°РµРј РІРІРѕРґ
 	string s;
+	int i;
+	DynamicArr* arr = new DynamicArr();
 	while (getline(cin, s)) {
 		if (s.empty()) {
 			break;
 		}
 		else {
 			int a = std::stoi(s);
-			mySeq->AddElement(a);
+			arr->Append(a);
 		}
 	}
-	//Считаем количество инверсий и выводим
-	cout << mySeq->CountInversions();
-	//чистим
-	delete mySeq;
+	//РЎС‡РёС‚Р°РµРј РєРѕР»РёС‡РµСЃС‚РІРѕ РёРЅРІРµСЂСЃРёР№ Рё РІС‹РІРѕРґРёРј
+	cout << ModifiedMergeSort(arr->GetArray(),0, arr->GetSize());
+	cout << arr->GetArray();
+	//cout << mySeq->CountInversions();
+	//С‡РёСЃС‚РёРј
+	delete arr;
 	return 0;
 }

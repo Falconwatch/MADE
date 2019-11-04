8238c4783b188e1ad1f699007f3ac2c2112ee88e
Falconwatch <garrikwower@gmail.com>
2019-11-04 19:51:26 +0300

    generic

--- Algo/w3/HW3/6_2/Source.cpp
+++ Algo/w3/HW3/6_2/Source.cpp
@@ -5,72 +5,99 @@
 using namespace std;
 
 #pragma region Node
+class IsLessInt {
+public:
+	IsLessInt() {};
+	bool operator ()(int l, int r) {
+		return l < r;
+	}
+};
+
+template<class T>
 class TreeNode
 {
 public:
-	TreeNode(int d);
-	~TreeNode();
-	bool IsGivenDataLess(int data);
-	TreeNode* GetLeftChild();
-	TreeNode* GetRightChild();
-	void SetLeftChild(TreeNode* child);
-	void SetRightChild(TreeNode* child);
-	int GetData();
+	TreeNode<T>(T d);
+	~TreeNode<T>();
+	template<class T>
+	bool IsGivenDataLess(T data);
+	TreeNode<T>* GetLeftChild();
+	TreeNode<T>* GetRightChild();
+	void SetLeftChild(TreeNode<T>* child);
+	void SetRightChild(TreeNode<T>* child);
+	T GetData();
 
 private:
-	int data_;
+	T data_;
 	TreeNode* left_;
 	TreeNode* right_;
 };
 
-TreeNode::TreeNode(int d) : data_(d), left_(nullptr), right_(nullptr) {}
-TreeNode::~TreeNode() {
+template<class T>
+TreeNode<T>::TreeNode(T d) : data_(d), left_(nullptr), right_(nullptr) {};
+
+template<class T>
+TreeNode<T>::~TreeNode<T>() {
 	if (left_ !=nullptr)
 		delete left_;
 	if (right_ != nullptr)
 		delete right_;
 }
 
-bool TreeNode::IsGivenDataLess(int data) {
-	return data < data_;
-}
+template<class T>
+TreeNode<T>* TreeNode<T>::GetLeftChild() { return left_; }
 
-TreeNode* TreeNode::GetLeftChild() { return left_; }
+template<class T>
+TreeNode<T>* TreeNode<T>::GetRightChild() { return right_; }
 
-TreeNode* TreeNode::GetRightChild() { return right_; }
+template<class T>
+void TreeNode<T>::SetLeftChild(TreeNode<T>* child) { left_ = child; }
 
-void TreeNode::SetLeftChild(TreeNode* child) { left_ = child; }
+template<class T>
+void TreeNode<T>::SetRightChild(TreeNode<T>* child) { right_ = child; }
 
-void TreeNode::SetRightChild(TreeNode* child) { right_ = child; }
-
-int TreeNode::GetData() { return data_; }
+template<class T>
+T TreeNode<T>::GetData() { return data_; }
 #pragma endregion
 
 
 #pragma region Tree
+template<class T, class IsLess>
 class Tree {
 	public:
-		Tree() : root_(nullptr) {}
-		~Tree() { delete root_; }
-		void AddNode(int data);
-		vector<TreeNode*> InOrder();
+		Tree<T, IsLess>();
+		~Tree<T, IsLess>();
+		void AddNode(T data);
+		vector<TreeNode<T>*> InOrder();
 	private:
-		TreeNode* root_;
+		TreeNode<T>* root_;
+		IsLess isless_;
 };
 
-void Tree::AddNode(int data) {
+template<class T, class IsLess>
+Tree<T,IsLess>::Tree<T,IsLess>() : root_(nullptr) {}
+
+template<class T, class IsLess>
+Tree<T, IsLess>::~Tree<T, IsLess>(){
+	if (root_ != nullptr) {
+		delete root_;
+	}
+}
+
+template<class T, class IsLess>
+void Tree<T, IsLess>::AddNode(T data) {
 	//дерево пустое
 	if (root_ == nullptr) {
-		root_ = new TreeNode(data);
+		root_ = new TreeNode<T>(data);
 		return;
 	}
 
-	TreeNode* current_node = root_;
+	TreeNode<T>* current_node = root_;
 	while (true) {
-		if (current_node->IsGivenDataLess(data)) {
-			TreeNode* left_child = current_node->GetLeftChild();
+		if (isless_(data, current_node->GetData())){
+			TreeNode<T>* left_child = current_node->GetLeftChild();
 			if (left_child == nullptr){
-				current_node->SetLeftChild(new TreeNode(data));
+				current_node->SetLeftChild(new TreeNode<T>(data));
 				return;
 			}
 			else {
@@ -78,9 +105,9 @@ void Tree::AddNode(int data) {
 			}
 		}
 		else {
-			TreeNode* right_child = current_node->GetRightChild();
+			TreeNode<T>* right_child = current_node->GetRightChild();
 			if (right_child == nullptr) {
-				current_node->SetRightChild(new TreeNode(data));
+				current_node->SetRightChild(new TreeNode<T>(data));
 				return;
 			}
 			else {
@@ -90,10 +117,11 @@ void Tree::AddNode(int data) {
 	}
 }
 
-vector<TreeNode*> Tree::InOrder() {
-	TreeNode* current_node = root_;
-	stack<TreeNode*> result;
-	vector<TreeNode*> result2;
+template<class T, class IsLess>
+ vector<TreeNode<T>*> Tree<T, IsLess>::InOrder() {
+	TreeNode<T>* current_node = root_;
+	stack<TreeNode<T>*> result;
+	vector<TreeNode<T>*> result2;
 	while (current_node != nullptr || result.empty() == false)
 	{
 		while (current_node != nullptr)
@@ -110,16 +138,16 @@ vector<TreeNode*> Tree::InOrder() {
 }
 #pragma endregion
 
-
-void ShowInOrder(Tree* tree) {
-	vector<TreeNode*> res = tree->InOrder();
+ template<class T, class IsLess>
+void ShowInOrder(Tree<T, IsLess>* tree) {
+	vector<TreeNode<T>*> res = tree->InOrder();
 	for (int i = 0; i < res.size(); i++) {
 		cout<<res[i]->GetData()<<" ";
 	}
 }
 
 int main() {
-	Tree* myTree= new Tree();
+	Tree<int, IsLessInt>* myTree= new Tree<int, IsLessInt>();
 	int n = 0;
 	cin >> n;
 	for (int i = 0; i < n; i++) {

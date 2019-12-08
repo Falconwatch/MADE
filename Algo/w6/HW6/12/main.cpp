0668a2cfb4806a2acc87381d2b0031d311d6c1bf
Igor <iashcherbakov@yandex.ru>
2019-12-08 12:01:41 +0300

    12 refactoring

--- Algo/w6/HW6/12/main.cpp
+++ Algo/w6/HW6/12/main.cpp
@@ -8,18 +8,19 @@
 
 using namespace std;
 
-
 class Graph {
 public:
 	Graph(int n) {
-		g_.resize(n + 1); used_.resize(n + 1);
-		d_.resize(n + 1); up_.resize(n + 1);
-		edges_added = 0;
+		g_.resize(n + 1); 
+		used_.resize(n + 1);
+		disc_.resize(n + 1); 
+		up_.resize(n + 1);
+		edges_added_ = 0;
 	};
 
 	void AddEdge(int a, int b) {
 		g_[a].push_back(b); g_[b].push_back(a);
-		edge_numbers[CreateEdge(a, b)].push_back(++edges_added);
+		edge_numbers_[CreateEdge(a, b)].push_back(++edges_added_);
 	}
 
 	set<int> FindBridges()
@@ -28,15 +29,15 @@ public:
 		for (int i = 1; i < n; i++)
 			if (!used_[i]) 
 				DFS(i);
-		return Bridges;
+		return bridges_;
 	}
 
 private:
 	vector<vector<int>> g_;
-	vector<int> used_, d_, up_;
-	set<int> Bridges;
-	map<pair<int, int>, vector<int>> edge_numbers;
-	int edges_added;
+	vector<int> used_, disc_, up_;
+	set<int> bridges_;
+	map<pair<int, int>, vector<int>> edge_numbers_;
+	int edges_added_;
 
 	pair<int, int> CreateEdge(int a, int b)
 	{
@@ -47,24 +48,24 @@ private:
 	void DFS(int v, int p = -1)
 	{
 		static int time = 1;
-		int i, to;
+		int to;
 		used_[v] = 1;
-		d_[v] = up_[v] = time++;
-		for (i = 0; i < g_[v].size(); i++)
+		disc_[v] = up_[v] = time++;
+		for (int i = 0; i < g_[v].size(); i++)
 		{
 			to = g_[v][i];
 			if (to == p)  continue;
 			if (used_[to])
-				up_[v] = min(up_[v], d_[to]);
+				up_[v] = min(up_[v], disc_[to]);
 			else
 			{
 				DFS(to, v);
 				up_[v] = min(up_[v], up_[to]);
-				if (up_[to] > d_[v]) {
-					vector<int> one_edge_numbers = edge_numbers[CreateEdge(v, to)];
+				if (up_[to] > disc_[v]) {
+					vector<int> one_edge_numbers = edge_numbers_[CreateEdge(v, to)];
 					if (one_edge_numbers.size() == 1) //если ребро кратное, то не мост!
 					{
-						Bridges.insert(one_edge_numbers[0]);
+						bridges_.insert(one_edge_numbers[0]);
 					}
 				}
 			}

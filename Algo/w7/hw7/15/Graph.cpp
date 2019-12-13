c30406150c16895c90946b6cce417672e3cbd3f6
Falconwatch <garrikwower@gmail.com>
2019-12-13 10:12:29 +0300

    15 refactored

--- Algo/w7/hw7/15/Graph.cpp
+++ Algo/w7/hw7/15/Graph.cpp
@@ -9,6 +9,7 @@
 #include<iostream>
 #include<math.h>
 #include"Graph.h"
+#include"Components.h"
 
 
 #pragma region Graph
@@ -102,39 +103,5 @@ double Graph::BruteForce() {
 }
 #pragma endregion
 
-#pragma region Components
-Components::Components(int n) : n(n)
-{
-	parents = new int[n + 1];
-	ranks = new int[n + 1];
 
-	for (int i = 0; i <= n; i++)
-	{
-		ranks[i] = 0;
-		parents[i] = i;
-	}
-}
-
-int Components::Find(int u)
-{
-	//ищем родителя заданной вершины рекурсивно
-	if (u != parents[u])
-		parents[u] = Find(parents[u]);
-	return parents[u];
-}
-
-void Components::Merge(int x, int y)
-{
-	x = Find(x), y = Find(y);
-
-	//присоединяем маньшее дерево к большему
-	if (ranks[x] > ranks[y])
-		parents[y] = x;
-	else
-		parents[x] = y;
-
-	if (ranks[x] == ranks[y])
-		ranks[y]++;
-}
-#pragma endregion
 
--- Algo/w7/hw7/15/Graph.h
+++ Algo/w7/hw7/15/Graph.h
@@ -23,15 +23,4 @@ private:
 	vector<Vertice> vertices;
 };
 
-// דנאפ נאחבטעûי םא ןמהדנאפû
-class Components
-{
-public:
-	Components(int n);
-	int Find(int u);
-	void Merge(int x, int y);
-private:
-	int* parents, * ranks;
-	int n;
-};
 
